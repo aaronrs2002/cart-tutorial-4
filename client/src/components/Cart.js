8ae6f83f862f661ca862dc3a7b482241ad0de745
aaronrs2002 <aaronrs2002@yahoo.com>
2022-04-19 18:19:16 -0700

    Update Cart.js
    Utilizing the local storage turn the price into a string. it needed to be switched back to a number.

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -16,7 +16,7 @@ const Cart = (props) => {
     const calculate = (tempCart) => {
         let tempPreTax = 0;
         for (let i = 0; i < tempCart.length; i++) {
-            tempPreTax = tempPreTax + tempCart[i].price;
+            tempPreTax = Number(tempPreTax) + Number(tempCart[i].price);
         }
         setPreTax((preTax) => tempPreTax);
     }
